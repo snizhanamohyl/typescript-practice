d6a90e9f5a400ac3f125427accf301145295561f
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-09-17 23:43:25 +0300

    lesson 3: array, objects types

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+// chapter 2
 let myName = 'Snezha';
 let meaningOfLife;
 let isLoading;
@@ -13,3 +14,52 @@ const sum = (a, b) => {
 let postId;
 let isActive;
 let re = /\w+/g;
+// chapter 3
+let stringArr = ['hello', 'world', 'daniia'];
+let guitars = ['ghost', 'core', 1908];
+const mixedArr = ['hello', 42, false];
+stringArr[2] = 'false';
+guitars = stringArr;
+guitars.unshift('false');
+const test = [];
+const bands = [];
+bands.push('post');
+let myTuple = ['dave', 42, true];
+let mix = ['sobd', 40, false];
+mix = myTuple;
+myTuple[0] = '89';
+// objects
+let obj;
+obj = {};
+obj = [];
+const exampleObj = {
+    name: 'dave',
+    year: 2000,
+};
+exampleObj.name = '2000';
+let evh = {
+    name: 'dave',
+    active: true,
+    albums: ['Summer night', "mid July", 2013],
+};
+let jp = {
+    name: 'john',
+    albums: ['Autumn night', "mid August", 2014],
+};
+const greetGuitarist = (quitarist) => {
+    var _a;
+    if (quitarist.active) {
+        return `Hello, ${(_a = quitarist.name) === null || _a === void 0 ? void 0 : _a.toUpperCase()}`;
+    }
+    return quitarist.name;
+};
+console.log(greetGuitarist(jp));
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.A);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// chapter 2
 let myName: string = 'Snezha';
 let meaningOfLife: number;
 let isLoading: boolean;
@@ -16,3 +17,71 @@ let postId: string | number;
 let isActive: boolean | number;
 
 let re: RegExp = /\w+/g
+
+// chapter 3
+
+let stringArr = ['hello', 'world', 'daniia'];
+let guitars = ['ghost', 'core', 1908]
+const mixedArr = ['hello', 42, false];
+
+stringArr[2] = 'false';
+guitars = stringArr;
+guitars.unshift('false');
+
+const test = [];
+const bands: string[] = [];
+bands.push('post');
+
+let myTuple: [string, number, boolean] = ['dave', 42, true]; 
+let mix = ['sobd', 40, false];
+mix = myTuple;
+myTuple[0] = '89';
+
+// objects
+let obj: object;
+obj = {};
+obj = [];
+
+const exampleObj = {
+    name: 'dave',
+    year: 2000,
+}
+exampleObj.name = '2000';
+
+type Guitarist = {
+    name: string,
+    active?: boolean,
+    albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+    name: 'dave',
+    active: true,
+    albums: ['Summer night', "mid July", 2013],
+}
+
+let jp: Guitarist = {
+    name: 'john',
+    albums: ['Autumn night', "mid August", 2014],
+}
+
+const greetGuitarist = (quitarist: Guitarist) => {
+    if (quitarist.active) {
+        return `Hello, ${quitarist.name?.toUpperCase()}`;
+    }
+
+    return quitarist.name;
+};
+
+console.log(greetGuitarist(jp));
+
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.A);
+
